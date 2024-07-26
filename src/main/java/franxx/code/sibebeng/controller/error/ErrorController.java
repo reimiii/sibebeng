852693f24ff3d527196f5654b68a0d8d2785bdcb
Hilmi AM <imiia75775@proton.me>
2024-07-26 21:52:26 +0700

    add controller advice untuk error hadling

---
+++ src/main/java/franxx/code/sibebeng/controller/error/ErrorController.java
@@ -0,0 +1,37 @@
+package franxx.code.sibebeng.controller.error;
+
+import franxx.code.sibebeng.dto.WebResponse;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ErrorController {
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<WebResponse<String>> constraintViolation(ConstraintViolationException violationException) {
+    List<String> errors = violationException.getConstraintViolations().stream()
+        .map(ConstraintViolation::getMessage)
+        .collect(Collectors.toList());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(WebResponse.<String>builder().message("validation errors").errors(errors).build());
+  }
+
+  @ExceptionHandler(ResponseStatusException.class)
+  public ResponseEntity<WebResponse<String>> apiException(ResponseStatusException exception) {
+    List<String> errors = new ArrayList<>();
+    if (exception.getReason() != null) {
+      errors.add(exception.getReason());
+    }
+    return ResponseEntity.status(exception.getStatusCode())
+        .body(WebResponse.<String>builder().errors(errors).build());
+  }
+}
