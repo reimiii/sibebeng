28819d1bd83eb58fba6b29d66d5db13f754a2e27
Hilmi AM <imiia75775@proton.me>
2024-08-05 16:22:51 +0700

    test created success

--- src/test/java/franxx/code/sibebeng/controller/VehicleControllerTest.java
+++ src/test/java/franxx/code/sibebeng/controller/VehicleControllerTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import franxx.code.sibebeng.dto.WebResponse;
 import franxx.code.sibebeng.dto.vehicle.request.CreateVehicleRequest;
+import franxx.code.sibebeng.dto.vehicle.response.VehicleResponse;
 import franxx.code.sibebeng.entity.Customer;
 import franxx.code.sibebeng.entity.Vehicle;
 import franxx.code.sibebeng.repository.CustomerRepository;
@@ -98,6 +99,40 @@ class VehicleControllerTest {
       System.out.println(objectMapper.writeValueAsString(response));
       assertThat(response.getErrors()).isNotEmpty();
       assertThat(response.getData()).isNull();
+
+    });
+  }
+  @Test
+  void createSuccess() throws Exception {
+    var request = CreateVehicleRequest.builder()
+        .licensePlate("F 0011 PP")
+        .brand("Suzuki")
+        .model("Carry")
+        .color("Blue Yellow")
+        .year("1020")
+        .build();
+
+    assertThat(customerRepository.existsById(customer.getId())).isTrue();
+
+    mockMvc.perform(
+        post("/api/customers/" + customer.getId() + "/vehicles")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request))
+    ).andExpectAll(
+        status().isCreated()
+    ).andDo(result -> {
+      WebResponse<VehicleResponse, Void> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+      });
+      System.out.println(objectMapper.writeValueAsString(response));
+
+      assertThat(vehicleRepository.existsById(response.getData().getId())).isTrue();
+      long count = vehicleRepository.findAllByCustomer_Id(customer.getId()).size();
+      assertThat(count).isEqualTo(2);
+
+      assertThat(response.getErrors()).isNull();
+      assertThat(response.getData()).isNotNull();
+      assertThat(response.getData().getBrand()).isEqualTo("Suzuki");
 
     });
   }
