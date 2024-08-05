732366965390f449e642144875e3e528041a4f5f
Hilmi AM <imiia75775@proton.me>
2024-08-05 12:38:10 +0700

    add some in vehicle repo

--- src/main/java/franxx/code/sibebeng/repository/VehicleRepository.java
+++ src/main/java/franxx/code/sibebeng/repository/VehicleRepository.java
@@ -1,9 +1,17 @@
 package franxx.code.sibebeng.repository;
 
+import franxx.code.sibebeng.entity.Customer;
 import franxx.code.sibebeng.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, String> {
+
+  Optional<Vehicle> findByCustomerAndId(Customer customer, String id);
+
+  List<Vehicle> findAllByCustomer(Customer customer);
 }
