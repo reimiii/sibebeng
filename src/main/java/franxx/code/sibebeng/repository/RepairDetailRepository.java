4d1c7d4f9b0cfc85f9bff49371cb4274510d2f01
Hilmi AM <imiia75775@proton.me>
2024-08-12 23:16:02 +0700

    add detail in entityFinder

--- src/main/java/franxx/code/sibebeng/repository/RepairDetailRepository.java
+++ src/main/java/franxx/code/sibebeng/repository/RepairDetailRepository.java
@@ -1,9 +1,14 @@
 package franxx.code.sibebeng.repository;
 
+import franxx.code.sibebeng.entity.Repair;
 import franxx.code.sibebeng.entity.RepairDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RepairDetailRepository extends JpaRepository<RepairDetail, String> {
+
+  Optional<RepairDetail> findByRepairAndId(Repair repair, String repairDetailId);
 }
--- src/main/java/franxx/code/sibebeng/service/EntityFinderService.java
+++ src/main/java/franxx/code/sibebeng/service/EntityFinderService.java
@@ -2,8 +2,10 @@ package franxx.code.sibebeng.service;
 
 import franxx.code.sibebeng.entity.Customer;
 import franxx.code.sibebeng.entity.Repair;
+import franxx.code.sibebeng.entity.RepairDetail;
 import franxx.code.sibebeng.entity.Vehicle;
 import franxx.code.sibebeng.repository.CustomerRepository;
+import franxx.code.sibebeng.repository.RepairDetailRepository;
 import franxx.code.sibebeng.repository.RepairRepository;
 import franxx.code.sibebeng.repository.VehicleRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,21 +21,28 @@ public class EntityFinderService {
   private final CustomerRepository customerRepository;
   private final VehicleRepository vehicleRepository;
   private final RepairRepository repairRepository;
+  private final RepairDetailRepository repairDetailRepository;
 
   public Customer findCustomer(String customerId) {
     return customerRepository.findById(customerId)
-        .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Customer not found"));
+        .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "customer not found"));
   }
 
   public Vehicle findVehicle(String customerId, String vehicleId) {
     var customer = findCustomer(customerId);
     return vehicleRepository.findByCustomerAndId(customer, vehicleId)
-        .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Vehicle not found"));
+        .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "vehicle not found"));
   }
 
   public Repair findRepair(String customerId, String vehicleId, String repairId) {
     var vehicle = findVehicle(customerId, vehicleId);
     return repairRepository.findByVehicleAndId(vehicle, repairId)
-        .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Repair not found"));
+        .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "repair not found"));
+  }
+
+  public RepairDetail findRepairDetail(String customerId, String vehicleId, String repairId, String repairDetailId) {
+    var repair = findRepair(customerId, vehicleId, repairId);
+    return repairDetailRepository.findByRepairAndId(repair, repairDetailId)
+        .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "repair detail not found"));
   }
 }
