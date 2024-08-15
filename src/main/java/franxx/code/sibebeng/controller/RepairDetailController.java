0e8d627655d3560f7b3cd8a3f23f74a2fb4618f4
Hilmi AM <imiia75775@proton.me>
2024-08-15 08:56:19 +0700

    update delete, ga butuh get karna udh ada di repair

--- src/main/java/franxx/code/sibebeng/controller/RepairDetailController.java
+++ src/main/java/franxx/code/sibebeng/controller/RepairDetailController.java
@@ -2,16 +2,15 @@ package franxx.code.sibebeng.controller;
 
 import franxx.code.sibebeng.dto.WebResponse;
 import franxx.code.sibebeng.dto.repairdetail.request.CreateRepairDetailRequest;
+import franxx.code.sibebeng.dto.repairdetail.request.UpdateRepairDetailRequest;
 import franxx.code.sibebeng.dto.repairdetail.response.RepairDetailResponse;
 import franxx.code.sibebeng.service.RepairDetailService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.http.HttpStatus.*;
-import static org.springframework.http.MediaType.*;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController @RequiredArgsConstructor
 @RequestMapping(
@@ -45,5 +44,56 @@ public class RepairDetailController {
     return ResponseEntity
         .status(CREATED)
         .body(response);
+  }
+
+  @PatchMapping(
+      path = "/{repairDetailId}",
+      consumes = APPLICATION_JSON_VALUE,
+      produces = APPLICATION_JSON_VALUE
+  )
+  public ResponseEntity<?> update(
+      @PathVariable(name = "customerId") String customerId,
+      @PathVariable(name = "vehicleId") String vehicleId,
+      @PathVariable(name = "repairId") String repairId,
+      @PathVariable(name = "repairDetailId") String repairDetailId,
+      @RequestBody UpdateRepairDetailRequest request
+  ) {
+
+    request.setCustomerId(customerId);
+    request.setVehicleId(vehicleId);
+    request.setRepairId(repairId);
+    request.setRepairDetailId(repairDetailId);
+
+    var rd = repairDetailService.updateRepairDetail(request);
+
+    var response = WebResponse.<RepairDetailResponse, Void>builder()
+        .message("repair detail updated successfully")
+        .data(rd)
+        .build();
+
+    return ResponseEntity.ok(response);
+  }
+
+  @DeleteMapping(
+      path = "/{repairDetailId}",
+      produces = APPLICATION_JSON_VALUE
+  )
+  public ResponseEntity<?> delete(
+      @PathVariable(name = "customerId") String customerId,
+      @PathVariable(name = "vehicleId") String vehicleId,
+      @PathVariable(name = "repairId") String repairId,
+      @PathVariable(name = "repairDetailId") String repairDetailId
+  ) {
+
+    repairDetailService.deleteRepairDetail(
+        customerId, vehicleId, repairId, repairDetailId
+    );
+
+    var response = WebResponse.<String, Void>builder()
+        .message("repair detail deleted successfully")
+        .data("OK")
+        .build();
+
+    return ResponseEntity.ok(response);
   }
 }
